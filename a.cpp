12be9adfb0e8c630f58cb67b0b7a912b2bf0583e
AmirNikravan <90094668+AmirNikravan@users.noreply.github.com>
2022-01-04 17:38:56 +0330

    Create a.cpp

---
+++ a.cpp
@@ -0,0 +1,16 @@
+/*
+Developer : AmirHosein Nikravan
+Shomare dnaeshjooye : 39914141054085
+Class : seshanbe Saate 18:00
+*/
+#include <iostream>
+#include <string>
+#include <limits>
+namespace
+{
+    constexpr unsigned count = 40u; // tedad karmand ha
+    static int __karmand__ = 0;     // tedad karmand haye ezafe shode
+    using ull = unsigned long long; // type alias
+}
+
+struct Employee
